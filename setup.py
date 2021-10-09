5f713e2e9cb077c8c4e41f671aecf49125b7314b
AhmetGurbuz <grbz523@gmail.com>
2021-10-09 21:25:47 +0300

    download_url

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='modeldeployer',
     packages=['modeldeployer'],
-    version='0.1.0',
+    version='0.1.1',
     license='apache-2.0',
     description='model deployer',
     author='Ahmet Gürbüz',
     author_email='ahmet.gurbuzz96@gmail.com',
     url='https://github.com/AhmetGurbuzz/model-deployer',
-    download_url='https://github.com/AhmetGurbuzz/model-deployer/archive/refs/heads/main.zip',
+    download_url='https://github.com/AhmetGurbuzz/model-deployer/archive/refs/tags/v0.1.1.tar.gz',
     keywords=['model', 'deployment', 'pickling', 'lambda'],
     install_requires=[
         'cloudpickle'
