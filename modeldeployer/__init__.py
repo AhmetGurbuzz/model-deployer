336dc98f092608b47bd30843f0dae2cde5de32a2
lastsweetpotato <grbz523@gmail.com>
2021-10-09 20:42:20 +0300

    Initial commit

---
+++ modeldeployer/__init__.py
@@ -0,0 +1,14 @@
+from modeldeployer.model_deployer import ModelDeployer
+from modeldeployer.util import load_model, save_model
+from modeldeployer.exceptions import IsNotList, IsNotFunction
+
+import pickle
+import cloudpickle
+
+
+PICKLE = 'pickle'
+CLOUDPICKLE = 'cloudpickle'
+pickling = {
+    PICKLE: pickle,
+    CLOUDPICKLE: cloudpickle
+}
---
+++ modeldeployer/util.py
@@ -0,0 +1,14 @@
+from modeldeployer import pickling, PICKLE
+
+
+def load_model(file_name, pickling_type=PICKLE):
+    if pickling.get(pickling_type):
+        with open(file_name, 'rb') as f_in:
+            model = pickling[pickling_type].load(f_in)
+        return model
+
+
+def save_model(model, file_name, pickling_type=PICKLE):
+    if pickling.get(pickling_type):
+        with open(file_name, 'wb') as f_out:
+            pickling[pickling_type].dump(model, f_out)
