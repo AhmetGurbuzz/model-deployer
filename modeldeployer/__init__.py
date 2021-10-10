e3932889ca54ca87c9856884262a3f2c2bc5008c
AhmetGurbuz <grbz523@gmail.com>
2021-10-10 15:13:30 +0300

    type checks for attributes and exception classes

--- modeldeployer/__init__.py
+++ modeldeployer/__init__.py
@@ -1,1 +1,14 @@
-from modeldeployer import ModelDeployer
+from modeldeployer.model_deployer import ModelDeployer
+from modeldeployer.util import load_model, save_model
+from modeldeployer.exception import IsNotList, IsNotFunction
+
+import pickle
+import cloudpickle
+
+
+PICKLE = 'pickle'
+CLOUDPICKLE = 'cloudpickle'
+pickling = {
+    PICKLE: pickle,
+    CLOUDPICKLE: cloudpickle
+}
