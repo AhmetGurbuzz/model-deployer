7db44effcb86daded62ebd06a92cb7cf9f33f574
AhmetGurbuz <grbz523@gmail.com>
2021-10-10 15:13:30 +0300

    type checks for attributes and exception classes

--- modeldeployer/ModelDeployer.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-class ModelDeployer:
-    def __init__(self, features: List[str], predict, calibrate):
-        self.features = features
-        self.predict = predict
-        self.calibrate = calibrate
